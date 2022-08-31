4f8e3a8729efb24ba089b1f70528c42ce8eed2e9
Alessandro-Salerno <OxyGen05@outlook.it>
2022-08-31 16:08:25 +0200

    Code improvements

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ let MSG_HTML;
 // Connect to the server
 function connectToServer() {
     // Read host address and username from input areas
-    const address = document.getElementById('host').value;
-    const name = document.getElementById('name').value;
+    const address = $('host').value;
+    const name = $('name').value;
 
     // Create the client instance
     client = new AromaClient(address, name);
@@ -77,11 +77,11 @@ function connectToServer() {
     // What happens when the client logs in
     client.addEventListener(AromaEvent.login, (event) => {
         // Update server name
-        document.getElementById('servername').innerText = event.serverName;
+        $('servername').innerText = event.serverName;
 
         // Update list of channels
         event.channels.forEach(element => {
-            document.getElementById('server').innerHTML += new ChannelButton(element).toHTML();
+            $('server').innerHTML += new ChannelButton(element).toHTML();
         });
 
         // Send message if enter is pressed
@@ -91,20 +91,20 @@ function connectToServer() {
                     sendMessage();
                 }
                 else {
-                    document.getElementById('newmessage').innerText += '\n';
+                    $('newmessage').innerText += '\n';
                 }
             }
         });
         
         // Show messageboard and focus input box
         showMessageboard();
-        document.getElementById('newmessage').focus();
-        MSG_HTML = document.getElementById('messages').innerHTML;;
+        $('newmessage').focus();
+        MSG_HTML = $('messages').innerHTML;;
     });
 
     client.addEventListener(AromaEvent.usermessage, (message) => {
         const msg = new Message(message.sender, message.content);
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
 
         if (message.sender != client.username) {
             const permission = Notification.requestPermission();
@@ -118,37 +118,37 @@ function connectToServer() {
 
     client.addEventListener(AromaEvent.userlogin, (event) => {
         const msg = new LoginMessage(event.name);
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     client.addEventListener(AromaEvent.userlogout, (event) => {
         const msg = new LogoutMessage(event.name);
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     client.addEventListener(AromaEvent.join, (event) => {
-        document.getElementById('messages').innerHTML = MSG_HTML;
+        $('messages').innerHTML = MSG_HTML;
         event.messages.forEach(message => { client.callEventListeners(message, message.type) });
-        document.getElementById(`${event.name}-button`).style.backgroundColor = 'cornflowerblue';
+        $(`${event.name}-button`).style.backgroundColor = 'cornflowerblue';
         const msg = new ChannelJoinMessage('You');
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     client.addEventListener(AromaEvent.leave, (event) => {
-        document.getElementById('messages').innerHTML = MSG_HTML;
-        document.getElementById(`${client.name}-button`).style = '';
+        $('messages').innerHTML = MSG_HTML;
+        $(`${client.name}-button`).style = '';
         const msg = new ChannelLeaveMessage('You');
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     client.addEventListener(AromaEvent.userjoin, (event) => {
         const msg = new ChannelJoinMessage(event.name);
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     client.addEventListener(AromaEvent.userleave, (event) => {
         const msg = new ChannelLeaveMessage(event.name);
-        document.getElementById('messages').innerHTML += msg.toHTML();
+        $('messages').innerHTML += msg.toHTML();
     });
 
     // Show the login form on disconnect
@@ -166,24 +166,24 @@ function connectToServer() {
 }
 
 function sendMessage() {
-    const message = document.getElementById('newmessage').value;
+    const message = $('newmessage').value;
     if (message == '') return;
     
     client.sendMessage(message);
-    document.getElementById('newmessage').value = '';
+    $('newmessage').value = '';
 }
 
 // Show the messageboard
 function showMessageboard() {
-    const form = document.getElementById('loginform');
-    const msgb = document.getElementById('messageboard');
+    const form = $('loginform');
+    const msgb = $('messageboard');
     form.style.display = 'none';
     msgb.style.display = 'flex';
 }
 
 function joinTextChannel(channel) {
     if (client.textChannel != null) {
-        document.getElementById(`${client.textChannel}-button`).style = '';
+        $(`${client.textChannel}-button`).style = '';
     }
     
     if (client.textChannel != channel) {
