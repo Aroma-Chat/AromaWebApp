d2221168a6963bfefcccec5c843d4007b4631d86
Ant00o <belliniantonio205@gmail.com>
2022-09-07 17:00:38 +0200

    Migliorata struttura delle variabili, e della login page

---
+++ js/cookies.js
@@ -0,0 +1,3 @@
+let cookie = document.cookie = 'cookieFirst=01; expires=Tue, 01 Jan 2023 00:00 UTC;';
+
+console.log(cookie);
---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            homeTitle: 'Welcome back to Aroma',
+            login: 'Login Account',
+            loginDescription: 'Fill the form below with your personal data, to accesso in your Aroma Account.',
+            hostName: 'Enter your host name',
+            password: 'Enter you password',
+        }
+    }
+})
